d89257a4898f72f8b5052dfb50c1616e0ddfd4ef
Xavier Lowe <43386971+xavierllowe@users.noreply.github.com>
2020-05-03 19:12:28 -0400

    Add files via upload

---
+++ Xavier's IBM Cert Project (1).py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook will be for the sole purpose of completeing the IBM capstone for the Data Science Professional Certficate.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+statement = "Hello Capstone Project Course!"
+print(statement)
+
+
+# In[ ]:
+
+
+
+
